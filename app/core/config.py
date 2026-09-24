1293922c173577710ba987007d2411394fcf5c86
abdoulacherif <abdoula13cherif@gmail.com>
2026-09-24 18:01:27 +0100

    Update config.py

--- app/core/config.py
+++ app/core/config.py
@@ -8,7 +8,6 @@ class Settings:
     LEEKPAY_PUBLIC_KEY: str = os.environ.get("LEEKPAY_PUBLIC_KEY", "")
     LEEKPAY_SECRET_KEY: str = os.environ.get("LEEKPAY_SECRET_KEY", "")
     LEEKPAY_API_URL: str = os.environ.get("LEEKPAY_API_URL", "https://leekpay.fr/api/v1")
-    LEEKPAY_WEBHOOK_SECRET: str = os.environ.get("LEEKPAY_WEBHOOK_SECRET", "")
 
     @property
     def is_leekpay_configured(self) -> bool:
